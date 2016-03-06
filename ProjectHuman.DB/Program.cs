f6f5eb3ebcc8a5e36fc8408394b1b2c89675aa4e
klonkr <anton.cornett@gmail.com>
2016-03-06 17:47:24 +0100

    removed uneccessary code from

--- ProjectHuman.DB/Program.cs
+++ ProjectHuman.DB/Program.cs
@@ -12,23 +12,6 @@ namespace ProjectHuman.DB
     {
         static void Main(string[] args)
         {
-            using (ProjectHumanContext context = new ProjectHumanContext())
-            {
-                //context.Humans.Add(new Human() {FirstName = "Anton"});
-                //context.Pets.Add(new Pet() {Name = "dogge", PetType =  new PetType() {Type = "Dog"}});
-
-                Human test = context.Humans.Where(u => u.FirstName == "Anne").First();
-                Human anton = context.Humans.First(u => u.FirstName == "Anton");
-
-                anton.MotherId = test.Id;
-
-
-                //Human anton = context.Humans.First(u => u.FirstName == "Anton");
-                //Human cr = context.Humans.First(u => u.FirstName == "Christopher-Robin");
-                //anton.Children.Add(cr);
-
-                context.SaveChanges();
-            }
         }
     }
 }
